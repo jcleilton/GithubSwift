ba2916985c27370a47a837e2904d10d6baabac67
jcleilton <josefeitosa@brq.com>
2020-11-13 12:09:17 -0300

    Initial Commit

---
+++ GithubSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubSwift
+//
+//  Created by Cleilton Feitosa on 13/11/20.
+//  Copyright © 2020 Cleilton Feitosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GithubSwiftTests/GithubSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  GithubSwiftTests.swift
+//  GithubSwiftTests
+//
+//  Created by Cleilton Feitosa on 13/11/20.
+//  Copyright © 2020 Cleilton Feitosa. All rights reserved.
+//
+
+import XCTest
+@testable import GithubSwift
+
+class GithubSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
